e05085a614e005177cc0dfd9d8fc1297435a30eb
jongwhan <jonghwan@crypto.com>
2022-07-25 17:06:49 +0900

    Problem: cannot set user-agent (fix #484) Solution: add set agent
    fix format
    
    fix udl
    
    fix wasm32
    
    fix url error
    
    add runtime setagent
    
    don't use mutex in wasm32
    
    fix clippy
    
    use once_cell
    
    fix typo
    
    fix clippy
    
    move once_cell crate
    
    tidy up feature guard
    
    fix target_arch
    
    fix wasm32 error
    
    add runtime setagent

---
+++ common/src/node/ethereum/provider.rs
@@ -0,0 +1,89 @@
+use crate::EthError;
+use ethers::providers::{Http, Provider};
+use url::Url;
+
+#[cfg(not(target_arch = "wasm32"))]
+use once_cell::sync::Lazy; // 1.3.1
+#[cfg(not(target_arch = "wasm32"))]
+use std::sync::{Arc, Mutex};
+
+#[cfg(not(target_arch = "wasm32"))]
+struct HttpClientInfo {
+    pub agentinfo: String,
+}
+#[cfg(not(target_arch = "wasm32"))]
+impl HttpClientInfo {
+    pub fn new() -> Self {
+        let useragentinfo = std::env::var("DEFIWALLETCORE_AGENTINFO");
+        if let Ok(agentinfo) = useragentinfo {
+            HttpClientInfo { agentinfo }
+        } else {
+            HttpClientInfo {
+                agentinfo: "defiwalletcore".to_string(),
+            }
+        }
+    }
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+static G_ETHERS_CLIENT_INFO: Lazy<Arc<Mutex<HttpClientInfo>>> =
+    Lazy::new(|| Arc::new(Mutex::new(HttpClientInfo::new())));
+
+// urlinfo: url string of the node to connect to, "http://mynode:8545"
+// agentinfo: agent string for http header
+pub async fn get_ethers_provider(urlinfo: &str) -> Result<Provider<Http>, EthError> {
+    let url = Url::parse(urlinfo).map_err(EthError::NodeUrl)?;
+
+    #[cfg(target_arch = "wasm32")]
+    let client = reqwest::Client::builder()
+        .build()
+        .map_err(EthError::ClientError)?;
+
+    #[cfg(not(target_arch = "wasm32"))]
+    let client = {
+        let info = G_ETHERS_CLIENT_INFO
+            .lock()
+            .expect("get HttpClientInfo lock");
+        let agentinfo = &info.agentinfo;
+        reqwest::Client::builder()
+            .user_agent(agentinfo)
+            .build()
+            .map_err(EthError::ClientError)?
+    };
+
+    let httpprovider = Http::new_with_client(url, client);
+    let finalprovider = Provider::new(httpprovider);
+    Ok(finalprovider)
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+pub fn set_ethers_httpagent(agentinfo: &str) -> Result<bool, EthError> {
+    let mut info = G_ETHERS_CLIENT_INFO
+        .lock()
+        .expect("get HttpClientInfo lock in set_ethers_httpagent");
+    info.agentinfo = agentinfo.to_string();
+    Ok(true)
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+pub fn get_ethers_httpagent() -> Result<String, EthError> {
+    let info = G_ETHERS_CLIENT_INFO
+        .lock()
+        .expect("get HttpClientInfo lock in get_ethers_httpagent");
+    Ok(info.agentinfo.clone())
+}
+
+#[cfg(test)]
+mod ethereum_provider_tests {
+    use super::*;
+
+    const TEST_AGENTINFO: &str = "testagentinfo";
+
+    #[cfg(not(target_arch = "wasm32"))]
+    #[test]
+    fn test_provider_agentinfo() {
+        set_ethers_httpagent(TEST_AGENTINFO).unwrap();
+        let value = get_ethers_httpagent().unwrap();
+        assert_eq!(value, TEST_AGENTINFO);
+    }
+}
