e0221f1afe4d1c8ee5df62cc61dff2b9b28f15f7
jongwhan <jonghwan@crypto.com>
2022-11-18 17:38:39 +0800

    send tx ok

--- example/cpp-example/main.cc
+++ example/cpp-example/main.cc
@@ -78,6 +78,8 @@ int main(int argc, char *argv[]) {
   // print mycronosrpc
   cout << "mycronosrpc " << mycronosrpc << endl;
   auto nonce1 = get_eth_nonce(mySender.c_str(), mycronosrpc);
+  // display nonce
+  cout << "nonce1 " << nonce1 << endl;
 
   
 
@@ -97,6 +99,32 @@ int main(int argc, char *argv[]) {
   cout<<hexstring<<endl;
   
   rust::String a=w->test();
+
+  char hdpath[100];
+  int cointype = 60;
+  int chainid = 1; // defined in cronos-devnet.yaml
+  snprintf(hdpath, sizeof(hdpath), "m/44'/%d'/0'/0/0", cointype);
+  Box<PrivateKey> privatekey = mywallet->get_key(hdpath);
+
+
+  EthTxInfoRaw eth_tx_info = new_eth_tx_info();
+  eth_tx_info.to_address = myReceiver.c_str();
+  eth_tx_info.nonce = nonce1;
+  eth_tx_info.amount = "0";
+  eth_tx_info.amount_unit = EthAmount::EthDecimal;
+  eth_tx_info.data= data;
+  eth_tx_info.gas_limit="219400";
+  eth_tx_info.gas_price="100000000";
+  eth_tx_info.gas_price_unit = EthAmount::WeiDecimal;
+
+
+  Vec<uint8_t> signedtx =
+      build_eth_signed_tx(eth_tx_info, chainid, true, *privatekey);
+  //U256 balance = get_eth_balance(myaddress1.c_str(), mycronosrpc);
+  String status =
+      broadcast_eth_signed_raw_tx(signedtx, mycronosrpc, 1000).status;
+  // print status
+  cout << "status: " << status << endl;
 
   
   cout<<"hello "<<a.c_str()<<endl;
