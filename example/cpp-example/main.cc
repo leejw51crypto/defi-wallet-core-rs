95f414f0186b7d5a2e6a6e67923679bbec15e940
jongwhan <jonghwan@crypto.com>
2022-11-18 15:47:52 +0800

    print hex

--- example/cpp-example/main.cc
+++ example/cpp-example/main.cc
@@ -16,6 +16,19 @@
 using namespace std;
 using namespace rust;
 using namespace org::defi_wallet_core;
+//  read bytes from char array and create hexstring
+std::string bytes_to_hexstring(const char *bytes, size_t len) {
+  ostringstream os;
+  os << hex << setfill('0');
+  for (size_t i = 0; i < len; ++i) {
+    os << setw(2) << static_cast<unsigned int>(static_cast<unsigned char>(bytes[i]));
+  }
+  return os.str();
+}
+
+
+
+
 int main(int argc, char *argv[]) {
   // read file MyErc721.json to string
   ifstream t("erc721-abi.json");
@@ -45,9 +58,11 @@ int main(int argc, char *argv[]) {
   w->add_address("0x8c8bdBe9CeE455732525086264a4Bf9Cf821C498"); // to
   w->add_uint("0"); // tokenId
    Vec<uint8_t> data=w->encode("safeTransferFrom"); // encoded 
-  for (int i = 0; i < data.size(); i++) {
-    cout << hex << (int)data[i] << " ";
-  }
+  std::string hexstring = bytes_to_hexstring((char *)data.data(), data.size());
+  // print data length
+  cout << "data length: " << data.size() << endl;
+  cout<<hexstring<<endl;
+  
   rust::String a=w->test();
 
   
