73617c2ca683674de7fc916c2df4175f2a427c3f
jongwhan <jonghwan@crypto.com>
2022-11-25 15:56:57 +0800

    fix

--- my/src/main.rs
+++ my/src/main.rs
@@ -54,51 +54,14 @@ async fn main() -> Result<()> {
         data: "1".to_string(),
     }];
     // [{"Uint":{"data":"1"}}]
-    // json encoding of params
     let json = serde_json::to_string(&params)?;
-    // print json
     println!("json: {}", json);
     let params2: Vec<EthAbiTokenBind> = serde_json::from_str(&json)?;
-    let mycall: ContractCall<_, MyDetokenizer> = contract.function_call2("ownerOf", params2)?;
+    let mycall: ContractCall<_, MyDetokenizer> = contract.function_call("ownerOf", params2)?;
     let feedback = mycall.call().await?;
     println!("mycall ok");
-    // debug print feedback
     println!("feedback: {:?}", feedback);
 
     Ok(())
 }
 
-#[tokio::main]
-async fn main2() -> Result<()> {
-    let abi_json = std::fs::read_to_string("../common/src/contract/erc721-abi.json")?;
-    let contract_address = std::env::var("MYCONTRACT721")?;
-    let rpc = std::env::var("MYCRONOSRPC")?;
-
-    let client = Provider::<Http>::try_from(rpc)?;
-    let contract = DynamicContract::new(&contract_address, &abi_json, client)?;
-    let tokens: Vec<Token> = vec![Token::Uint(1.into())];
-    let tokensjson = serde_json::to_string(&tokens)?;
-    let tokens2: Vec<Token> = serde_json::from_str(&tokensjson)?;
-    println!("tokens json: {:?}", tokens);
-    println!("tokens2 json: {:?}", tokens2);
-
-    let mydata = MyTokenizer { json: tokensjson };
-
-    let tokens = Token::Uint(1.into());
-    let json = serde_json::to_string(&tokens)?;
-    println!("tokens: {:?}", tokens);
-    println!("json: {}", json);
-
-    //return Ok(());
-
-    let params = MyTokenizer {
-        json: json.to_string(),
-    };
-    let mycontract: ContractCall<_, MyDetokenizer> = contract.function_call("ownerOf", mydata)?;
-    let feedback = mycontract.call().await?;
-    println!("mycall ok");
-    // debug print feedback
-    println!("feedback: {:?}", feedback);
-
-    Ok(())
-}
