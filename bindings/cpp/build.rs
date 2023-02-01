933a9fc72eb373eb4f8246c370c638ed33579ce1
jongwhan <jonghwan@crypto.com>
2023-02-01 17:54:13 +0900

    android c++ compile

--- bindings/cpp/build.rs
+++ bindings/cpp/build.rs
@@ -6,6 +6,27 @@ const BRIDGES: &[&str] = &[
     "src/uint.rs",
 ];
 
+
+
+#[cfg(target_os = "android")]
+fn main() {
+    cxx_build::CFG.doxygen = true;
+    cxx_build::bridges(BRIDGES)
+        .file("src/nft.cc")
+        .file("src/android.cc")
+        .flag_if_supported("-std=c++11")
+        .compile("defi_wallet_core");
+
+    for bridge in BRIDGES {
+        println!("cargo:rerun-if-changed={}", bridge);
+    }
+
+    println!("cargo:rerun-if-changed=src/nft.cc");
+    println!("cargo:rerun-if-changed=include/nft.h");
+}
+
+
+#[cfg(not(target_os = "android"))]
 fn main() {
     cxx_build::CFG.doxygen = true;
     cxx_build::bridges(BRIDGES)
